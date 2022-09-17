cf546a6bcf65c4f20aaa615e3ec61d74bb81986f
Jose Ricardo Vazquez <josev8212@gmail.com>
2022-09-17 18:16:20 -0500

    implemented some javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+const quoteContainer = document.getElementById(`quote-container`);
+
+const quoteText = document.getElementById(`quote`);
+const authorText = document.getElementById(`author`);
+const twitterBtn = document.getElementById(`twitter`);
+const newQuoteBtn = document.getElementById(`new-quote`);
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+	// Pick a random quote from apiQuotes array
+	const quote = apiQuotes[Math.random() * apiQuotes.lenght];
+
+	authorText.textContent = quote.author;
+	quoteText.textContent = quote.text;
+}
+
+//Get quote from API
+
+async function getQuotes() {
+	const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+	try {
+		const responce = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		//Handle Error Here
+	}
+}
+
+//onLoad
+getQuotes();
